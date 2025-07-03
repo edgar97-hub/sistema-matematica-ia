1ba2d6a5da77678b7eb328f7d7780f433bb4e0da
edgar97-hub <ed.quispe.mamani@isur.edu.pe>
2025-07-03 11:18:43 -0500

    .

--- backend/src/math-processing/services/audio.service.ts
+++ backend/src/math-processing/services/audio.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import * as mm from 'music-metadata';
+// import * as mm from 'music-metadata';
 
 @Injectable()
 export class AudioService {
   async getAudioDuration(audioBuffer: Buffer): Promise<number> {
     try {
-      const metadata = await mm.parseBuffer(audioBuffer, 'audio/mpeg');
-      return metadata.format.duration || 0;
+      // const metadata = await mm.parseBuffer(audioBuffer, 'audio/mpeg');
+      // return metadata.format.duration || 0;
+      return 1;
     } catch (error) {
       console.error('Error al obtener la duración del audio:', error);
       return 0;
